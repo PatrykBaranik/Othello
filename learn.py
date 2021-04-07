c4f2c51634ec38eb0f45b17de4e991b688d82818
PatrykBaranik <patrykbar97@gmail.com>
2021-04-08 00:19:43 +0200

    learn

--- learn.py
+++ learn.py
@@ -8,6 +8,8 @@ if l == 4:
     _, directory, n_games, minsc = sys.argv
 if l == 3:
     _, directory, layers = sys.argv
+    n_games = 100000
+    minsc = 900
 if l == 2:
     _, directory = sys.argv
     n_games = 100000
@@ -21,19 +23,21 @@ if os.path.isfile(directory+'/parameters.txt'):
     if os.path.isfile(directory+'/q_eval_dqn'):
         agent1.load_models()
 else:
-    agent1 = ptorch3.Agent(directory, layers)
-agent1.set_optimizer(0.999)
+    agent1 = ptorch3.Agent(directory, literal_eval(layers))
+agent1.set_optimizer(0.00005)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=10)
-agent1.set_optimizer(0.5)
+agent1.set_optimizer(0.0005)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=20)
-agent1.set_optimizer(0.2)
+agent1.set_optimizer(0.005)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=30)
-agent1.set_optimizer(0.1)
+agent1.set_optimizer(0.01)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=40)
 agent1.set_optimizer(0.05)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=50)
-agent1.set_optimizer(0.005)
+agent1.set_optimizer(0.1)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=60)
-agent1.set_optimizer(0.0005)
+agent1.set_optimizer(0.5)
 a = otorch.learn(n=n, directory=directory, n_games=int(n_games), net=agent1, save=True, minsc=800)
+
+
 
